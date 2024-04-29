40dc770d56fbe3241fc479154e77fd812b2e95c7
Felipe Pontes <107514967+Felipeppontes@users.noreply.github.com>
2024-04-29 12:18:22 -0300

    Update LE01_Q1_Felipe_Pontes.asm

--- LE01_Q1_Felipe_Pontes.asm
+++ LE01_Q1_Felipe_Pontes.asm
@@ -1,11 +1,11 @@
-		;Questão	1 feita por Felipe Pontes.
-		
+		;---------------------------Questão	1 ---------------------------------- [feita por Felipe Pontes]
+		;Q01)
 		;•		carregue os valores {0x12, 0x12AB, 0x12AB34 , 0x12AB34CD} nos registradores R0 ao R3, respectivamente;
 		;•		Após isso, salve esses registradores R0 ao R3 na memória de dados nos endereços 0x100 ao 0x10C, respectivamente;
 		;•		Por fim, leia e memória de dados e salve nos registradores R8 ao R11.
 		
 		;		Carregar valores divididos em registradores usando LSL, ADD e MOV
-		
+		;======================================================================================================================
 		
 		
 		
@@ -14,55 +14,53 @@
 		
 		
 		;---------Carregar	0x12 em R0 e R1 (dividido em duas partes de 8 bits)--------------
-		mov		R0, #0x00
-		mov		R1, #0x12
-		;		Carregar 0x12AB em R2 e R3 (dividido em duas partes de 8 bits)
-		mov		R2, #0x00
-		mov		R3, #0x12
-		lsl		R3, R3, #8     ; Desloca R3 8 bits para a esquerda
-		add		R3, R3, #0xAB ; Adiciona 0xAB a R3
-		;		Carregar 0x12AB34 em R2 e R3 (dividido em duas partes de 8 bits)
-		mov		R1, R3         ; copia valor de R3 para R1
-		mov		R2, R3         ; Copia valor de R3 para R2
-		lsl		R2, R2, #8     ; Desloca R2 8 bits para a esquerda
-		add		R2, R2, #0x34 ; Adiciona 0x34 a R2
-		mov		R3, #0x00
-		add		R3, R3, R1     ; Adiciona R1 a R3
+		mov		r0, #0x00
+		mov		r1, #0x12
+		;		carregar 0x12ab em r2 e r3
+		mov		r2, #0x00
+		mov		r3, #0x12
+		lsl		r3, r3, #8     ; Desloca r3 8 bits para a esquerda
+		add		r3, r3, #0xab ; adiciona 0xab a r3
+		;		carregar 0x12ab34 em r2 e r3
+		mov		r1, r3         ; Copia valor de r3 para r1
+		mov		r2, r3         ; Copia valor de r3 para r2
+		lsl		r2, r2, #8     ; desloca r2 8 bits para a esquerda
+		add		r2, r2, #0x34 ; adiciona 0x34 a r2
+		mov		r3, #0x00
+		add		r3, r3, r1     ; Adiciona r1 a r3
 		
-		;		Carregar 0x12AB34CD em R3 (dividido em duas partes de 8 bits)
-		mov		R2, R3         ; copia valor de R3 para R2
-		lsl		R2, R2, #8     ; Desloca R2 8 bits para a esquerda
-		add		R2, R2, #0xCD ; Adiciona 0xCD a R2
-		mov		R3, #0x00
-		add		R3, R3, R2     ; Adiciona R2 a R3
+		;		carregar 0x12ab34cd em r3
+		mov		r2, r3         ; copia valor de r3 para r2
+		lsl		r2, r2, #8     ; desloca r2 8 bits para a esquerda
+		add		r2, r2, #0xcd ; Adiciona 0xcd a r2
+		mov		r3, #0x00
+		add		r3, r3, r2     ; adiciona r2 a r3
+		sub		r2, r3, #0xcd  ; isso estava sobrando aqui
+		add		r2, r2, #0x34 ; e aqui também
+		mov		r3, r2         ; copia valor de r2 para r3
+		lsl		r3, r3, #8     ; desloca r3 8 bits para a esquerda
+		add		r3, r3, #0xcd ; adiciona 0xcd a r3
+		mov		r0, #0x12
+
 		
-		SUB		R2, R3, #0xCD  ; Isso estava sobrando aqui
-		ADD		R2, R2, #0x34 ; E aqui também
-		mov		R3, R2         ; Copia valor de R2 para R3
-		lsl		R3, R3, #8     ; Desloca R3 8 bits para a esquerda
-		add		R3, R3, #0xCD ; Adiciona 0xCD a R3
-		mov		R0, #0x12
+		;--------------salvar	valores na memória de dados
+		ldr		r4, =0x200     ; carregar endereço inicial da memória de dados em r4  ; professor eu não entendi muito bem o erro com o endereço 0x100, logo eu partir para o endereeeço 0x200 e segui o mesmo padrão
+		str		r0, [r4]       ; salvar r0 em 0x200
+		add		r4, r4, #4     ; incrementa memória em 4 bytes
+		str		r1, [r4]       ; salvar r1 em 0x204
+		add		r4, r4, #4     ; incrementar memória m 4 bytes
+		str		r2, [r4]       ; salvar r2 em 0x208
+		add		r4, r4, #4     ; incrementar dememória em 4 byte
+		str		r3, [r4]       ; salvar r3 em 0x20c
+
 		
-		
-		;--------------Salvar	valores na memória de dados-------------------------
-		ldr		R4, =0x200     ; Carregar endereço inicial da memória de dados em R4  ; PROFESSOR EU NÃO ENTENDI MUITO BEM O ERRO COM O ENDEREÇO 0X100, LOGO EU PARTIR PARA O ENDEREEÇO 0X200 E SEGUI O MESMO PADRÃO
-		
-		str		R0, [R4]       ; Salvar R0 em 0x200
-		add		R4, R4, #4     ; Incrementar o ponteiro de memória em 4 bytes
-		str		R1, [R4]       ; Salvar R1 em 0x204
-		add		R4, R4, #4     ; Incrementar o ponteiro de memória em 4 bytes
-		str		R2, [R4]       ; Salvar R2 em 0x208
-		add		R4, R4, #4     ; Incrementar o ponteiro de memória em 4 bytes
-		str		R3, [R4]       ; Salvar R3 em 0x20C
-		
-		
-		;-------------Ler	valores da memória de dados e salvar nos registradores ---------------
-		;		Carregar os valores da memória de dados para os registradores R8 ao R11
-		ldr		R4, =0x200     ; Carregar o endereço inicial da memória de dados em R4
-		ldr		R8, [R4]       ; Carregar o valor em 0x200 para R8
-		add		R4, R4, #4     ; Incrementar o ponteiro de memória em 4 bytes
-		ldr		R9, [R4]       ; Carregar o valor em 0x204 para R9
-		add		R4, R4, #4     ; Incrementar o ponteiro de memória em 4 bytes
-		ldr		R10, [R4]      ; Carregar o valor em 0x208 para R10
-		add		R4, R4, #4     ; Incrementar o ponteiro de memória em 4 bytes
-		ldr		R11, [R4]      ; Carregar o valor em 0x20C para R11
+		;-------------ler	valores da memória de dados e salvar nos registradores
+		;		carregar os valores da memória de dados para os registradores r8 ao r11
+		ldr		r4, =0x200     ; carregar o endereço inicial da memória de dados em r4
+		ldr		r8, [r4]       ; carregar o valor em 0x200 para r8
+		add		r4, r4, #4     ; incrementar memória em 4 bytes
+		ldr		r9, [r4]       ; carregar o valor em 0x204 para r9
+		add		r4, r4, #4     ; incrementar memória em 4 bytes
+		ldr		r10, [r4]      ; carregar o valor em 0x208 para r10
+		add		r4, r4, #4    
+		ldr		r11, [r4]      ; carregar o valor em 0x20c para r11
