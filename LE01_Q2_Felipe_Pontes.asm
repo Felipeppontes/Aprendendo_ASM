82c3c409f9b3dfde2786f2909dd5557a05e9a883
Felipe Pontes <107514967+Felipeppontes@users.noreply.github.com>
2024-04-29 12:11:42 -0300

    Update LE01_Q2_Felipe_Pontes.asm

--- LE01_Q2_Felipe_Pontes.asm
+++ LE01_Q2_Felipe_Pontes.asm
@@ -1,39 +1,38 @@
-		;=================QUEST√O	2 | Felipe Pontes | Sistemas Digitais 2 ===============================
+		;=================QUEST√ÉO	2 | Felipe Pontes | Sistemas Digitais 2 ===============================
 		
-		;Usando	laÁos: faÁa um cÛdigo que calcule os 30 primeiros
-		;termos	da sequÍncia de Fibonacci
-		;e		os guarde na memÛria de dados para isso utilize um laÁo,
-		;sabendo	que os dois primeiros termos da sequÍncia s„o
-		;F(0)	= 1 e F(1) = 1 e que o n-Èsimo termo È dado por
+		;Usando	la√ßos: fa√ßa um c√≥digo que calcule os 30 primeiros
+		;termos	da sequ√™ncia de Fibonacci
+		;e		os guarde na mem√≥ria de dados para isso utilize um la√ßo,
+		;sabendo	que os dois primeiros termos da sequ√™ncia s√£o
+		;F(0)	= 1 e F(1) = 1 e que o n-√©simo termo √© dado por
 		;F(n)	= F(n?1)+F(n?2).
+		;==============================================================================================
 		
-		
-		;Aprendendo	sobre operandos de memÛria:
-		
-		
-		
+		;Aprendendo	sobre operandos de mem√≥ria:
 		
 		
 		
-		mov		r0, #256         ; Inicializa o endereÁo de memÛria para armazenar os termos
-		mov		r1, #1         ; Inicializa o primeiro termo da sequÍncia (F(0))
-		mov		r2, #1         ; Inicializa o segundo termo da sequÍncia (F(1))
-		str		r1, [r0], #4  ; Armazena o primeiro termo na memÛria e incrementa o endereÁo
-		str		r2, [r0], #4   ; Armazena o segundo termo na memÛria e incrementa o endereÁo
-		mov		r3, #2         ; Inicializa o contador de termos
 		
-loop1
-		cmp		r3, #30        ; Compara o contador com 30
-		beq		loop2          ; Se atingir 30 termos, vai para loop2
-		add		r4, r1, r2     ; Calcula o prÛximo termo (F(n) = F(n-1) + F(n-2))
-		str		r4, [r0], #4  ; Armazena o prÛximo termo na memÛria e incrementa o endereÁo
-		mov		r1, r2         ; Atualiza F(n-2) para o valor de F(n-1)    ;Demorei para pegar essa lÛgica
-		mov		r2, r4         ; Atualiza F(n-1) para o valor calculado
-		add		r3, r3, #1     ; Incrementa o contador de termos
-		b		loop1          ; Volta para loop1
 		
+		
+		mov		r0, #256         ; Inicializa o endere√ßo de mem√≥ria para armazenar os termos --> 0x100
+		mov		r1, #1         ; Inicializa o primeiro termo da sequ√™ncia F(0)
+		mov		r2, #1         ;  segundo termo da sequ√™ncia F(1)
+		str		r1, [r0], #4  ; Armazena o primeiro na mem√≥ria e incrementa o endere√ßo --> 0x104
+		str		r2, [r0], #4   ; Armazena o segundo na memria e incrementa o endere√ßo
+		mov		r3, #2         ; Inicializa o contador de termos 
+		
+loop1	cmp		r3, #30        ; Compara o contador com 30
+;"Branch if Equal" (desvio se igual) deq
+		beq		loop2          ; Se atingir 30 termos, vai para loop2 -->
+		add		r4, r1, r2     ; Calcula o pr√≥ximo termo F(n) = F(n-1) + F(n-2)
+		str		r4, [r0], #4  ; armazena o pr√≥ximo termo na mem√≥ria e incrementa o endere√ßo
+		mov		r1, r2         ; Atualiza F(n-2) para o valor de F(n-1)    ;Demorei para pegar essa l√≥gica
+		mov		r2, r4         ; Atualiza F(n-1) para o valor calculadi
+		add		r3, r3, #1     ; Incrementa o contadoe
+		b		loop1        
 loop2
-		mov		r0, #0         ; Fim
+		mov		r0, #0         
 		
 		
 		
